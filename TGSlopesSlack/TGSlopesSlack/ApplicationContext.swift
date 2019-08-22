4dc364c7d15ebf0535322e6f61940b06e3f1d23c
Huang Yong Le <samadhihuang@gmail.com>
2019-08-22 15:43:56 +0800

    TGUIKit Test Success

---
+++ TGSlopesSlack/TGSlopesSlack/ApplicationContext.swift
@@ -0,0 +1,9 @@
+//
+//  ApplicationContext.swift
+//  TGSlopesSlack
+//
+//  Created by Buck on 2019/8/22.
+//  Copyright © 2019 Buck. All rights reserved.
+//
+
+import Foundation
---
+++ TGSlopesSlack/TGSlopesSlack/parent-controllers/ControllerExtension.swift
@@ -0,0 +1,9 @@
+//
+//  ControllerExtension.swift
+//  TGSlopesSlack
+//
+//  Created by Buck on 2019/8/22.
+//  Copyright © 2019 Buck. All rights reserved.
+//
+
+import Foundation
